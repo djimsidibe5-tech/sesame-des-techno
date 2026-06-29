7572eff5e684302c4947ecfe7205bb77d85f18a1
djimsidibe <djimsidibe5@gmail.com>
2026-06-29 23:53:17 +0000

    réorganisation des dossiers et correction du code

---
+++ main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+# Montage des fichiers statiques
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Configuration des templates
+templates = Jinja2Templates(directory="templates")
+
+# Structure de vos données académiques
+ACADEMIC_DATA = {
+    "Licence 1": ["Mécanique du point", "Optique géométrique", "Thermodynamique", "Algèbre linéaire"],
+    "Licence 2": ["Électromagnétisme", "Ondes mécaniques", "Analyse numérique", "Physique statistique"],
+    "Licence 3": ["Physique atomique", "Physique du solide", "Mécanique analytique", "Relativité"],
+    "Master 1": ["Physique théorique", "Traitement du signal", "Méthodes numériques"],
+    "Master 2": ["Énergie", "Recherche bibliographique", "Thèse"]
+}
+
+@app.get("/")
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {
+        "request": request,
+        "data": ACADEMIC_DATA
+    })
+
+# Commande pour lancer : uvicorn main:app --reload
